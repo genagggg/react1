98a7bdc1b4a4cf0f56e68005808de1e3c42b43f5
gena_gorokhov <mr.genag@mail.ru>
2021-06-11 20:19:10 +0500

    make header and footer and content

--- src/App.js
+++ src/App.js
@@ -7,9 +7,24 @@ function App() {
       
         <header>
           <div className='container'>
-            
+          <div className='logo'></div> 
+          <nav>
+            <ul>
+              <li>Main</li>
+              <li>Blog</li>
+              <li>News</li>
+              <li>Contact</li>
+              <li>Roof</li>
+            </ul>
+            </nav> 
           </div>
         </header>
+        <div className='content'>
+          <div className='container'></div>
+        </div>
+        <footer>
+          <div className='container'></div>
+        </footer>
       
     </div>
   );
