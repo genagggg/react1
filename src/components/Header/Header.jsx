4e5404283db0d6d3d77505bf09646fd03c177fbc
gena_gorokhov <mr.genag@mail.ru>
2021-06-12 21:20:14 +0500

    I make Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import s from './Header.module.css';
+import Navbar from './Navbar/Navbar.jsx';
 const Header =()=> {
     return (
     <div>
           <header>
           <div className={s.container}>
           <div className={s.logo}></div> 
-          <nav>
-            <ul>
-              <li>Main</li>
-              <li>Blog</li>
-              <li>News</li>
-              <li>Contact</li>
-              <li>Roof</li>
-            </ul>
-            </nav> 
+          <Navbar />
           </div>
         </header>
     </div>
---
+++ src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import s from './Navbar.module.css';
+
+const Navbar =()=> {
+return (
+   
+          <nav>
+            <ul>
+              <li>Main</li>
+              <li>Blog</li>
+              <li>News</li>
+              <li>Contact</li>
+              <li>Roof</li>
+            </ul>
+           </nav> 
+  
+);
+}
+export default Navbar;
