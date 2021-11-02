5537e2c40c674362c1d16c4911c280421f7f1f60
gena_gorokhov <mr.genag@mail.ru>
2021-11-02 16:19:53 +0500

    delete map

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,16 +2,9 @@ import React from 'react';
 import s from './Blog.module.css';
 
 const Blog =()=> {
-let message = {
-  mess1:[
-    {id:1, name: 'Igor', age:23},
-    {id:2, name: 'Oleg', age: 34},
-    {id:3, name: 'Kolya', age: 45}
-  ]
-}
-let mess2 = message.mess1.map(m=><div>{m.id}</div>)
+
     return (<div>
-       <p className={s.color}>{mess2}</p> 
+       <p className={s.color}></p> 
        
          </div>);
     
