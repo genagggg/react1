59474f18d493877fedfa0ce75874d5fbf74cc488
gena_gorokhov <mr.genag@mail.ru>
2021-11-02 16:24:30 +0500

    Map Dep

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import s from './Blog.module.css';
 
 const Blog =()=> {
+let mess = {
+    mess1:[
+        {id: 1, name:'igor'},
+        {id: 2, name: 'jora'},
+        {id: 3, name: 'kolya'}
+    ]
+}
 
+let mess2 = mess.mess1.map(m=><div>{m.name}</div>)
     return (<div>
-       <p className={s.color}></p> 
+       <p className={s.color}>{mess2}</p> 
        
          </div>);
     
