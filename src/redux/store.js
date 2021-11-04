144afcbf8385fa0a4002df2c5a576f5004b87846
gena_gorokhov <mr.genag@mail.ru>
2021-11-04 21:09:53 +0500

    Add users-reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import {combineReducers,createStore} from 'redux';
 import mainReducer from './main-reducer';
+import usersReducer from './users-reducer';
 let reducers = combineReducers({
-mainPage: mainReducer
+mainPage: mainReducer,
+usersPage: usersReducer
 });
 
 let store = createStore(reducers);
---
+++ src/redux/users-reducer.js
@@ -0,0 +1,13 @@
+
+let initialState = {
+    users:[
+        {id: 1, name: 'Aleksandr', age: 23},
+        {id: 2, name: 'Yuriy', age: 32},
+        {id: 3, name: 'Grigoriy', age: 21}
+    ]
+}
+const usersReducer=(state=initialState, action)=>{
+return 0;
+}
+
+export default usersReducer;
