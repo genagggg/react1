18473cea337a60fcbd0fb33a3cda3ae87180a6ea
gena_gorokhov <mr.genag@mail.ru>
2021-11-04 21:24:32 +0500

    Add UsersContainer

---
+++ src/components/Users/UsersContainer.jsx
@@ -0,0 +1,15 @@
+import { connect } from "react-redux";
+import Users from "./Users";
+
+let mapStateToProps =(state)=>{
+    return {
+        users: state.usersPage.users
+    }
+}
+
+let mapDispatchToProps = (dispatch)=>{
+    return 0;
+}
+
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps) (Users);
+export default UsersContainer;
