829715bab690a575e63983f911480d5a07fdf9ad
gena_gorokhov <mr.genag@mail.ru>
2021-11-06 17:15:32 +0500

    Refff

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import s from "./Users.module.css";
 
 const Users =(props)=>{
+let gef=React.createRef();
+
+
+
+let alerTO =()=>{
+    let text = gef.current.value;
+    alert(text);
+}
+
+
     let use = props.users.map(u=><p>{u.name}</p>);
 return(
     <div>
-        {use}
+       <div>{use}</div> 
+
+       <div>
+           <textarea ref={gef} className={s.textarrea}></textarea>
+           <button onClick={alerTO}>Нажим</button>
+       </div>
     </div>
 )
 }
