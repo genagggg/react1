e547c00988352788aec62181ac7746b492fbed82
Gena <mr.genag@mail.ru>
2023-02-20 13:48:24 +0500

    Сделал счётчик и добавил бустрап

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Main.module.css';
 import MainButton from './MainButton/MainButton';
-const Main =(props)=> {
- 
+const Main = (props) => {
+
+    const [counter, setCounter] = useState(0)
+
+    function increment() {
+        setCounter(counter + 1)
+    }
+
+    function decrement() {
+        setCounter(counter - 1)
+    }
 
     return (
-<div>
-    {
-        props.users.map(u=><div key={userId}>
-            
-        </div>)
-    }
-</div>
+        <div>
+            <h1>Счётчик: {counter}</h1>
+            <button onClick={increment} className='btn btn-success'>Добавить</button>
+            <button onClick={decrement} className='btn btn-danger'>Убрать</button>
+        </div>
     );
 
 }
