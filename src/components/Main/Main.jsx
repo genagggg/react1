47f8605517157b996e9ce57e1af0205178470a86
Gena <mr.genag@mail.ru>
2023-02-20 16:17:47 +0500

    Добавил Материал ui

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,23 +1,43 @@
 import React, { useState } from 'react';
 import s from './Main.module.css';
 import MainButton from './MainButton/MainButton';
+import { Button } from '@mui/material';
+
+
+function completCounter() {
+    console.log("some calculation")
+    return Math.trunc(Math.random() * 20)
+}
+
 const Main = (props) => {
 
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(() => { return completCounter() })
 
     function increment() {
+
         setCounter(counter + 1)
+
     }
 
     function decrement() {
         setCounter(counter - 1)
     }
 
+    const [state, setState] = useState({
+        title: "Счётчик",
+        date: Date.now()
+    })
+
     return (
         <div>
             <h1>Счётчик: {counter}</h1>
             <button onClick={increment} className='btn btn-success'>Добавить</button>
             <button onClick={decrement} className='btn btn-danger'>Убрать</button>
+            <button onClick={() => setState({ title: 'Новое название' })} className='btn btn-danger'>Изменить название</button>
+            <Button variant="outlined">Outlined</Button>
+            <pre>
+                {JSON.stringify(state, null, 2)}
+            </pre>
         </div>
     );
 
